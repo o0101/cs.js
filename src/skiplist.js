d2b8c513b6888559191da008d437a9cf65d2f1bb
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-02-27 05:37:37 +0000

    "OK"

--- src/skiplist.js
+++ src/skiplist.js
@@ -6,6 +6,7 @@
 
 // constants
   const DEFAULT_OPTIONS = {
+    max: false,             /* increasing order, true gives decreasing order */
     p: 1/2,                 /* probability node lifts to higher levels */
     randomized: true,       /* if we base lifting on randomizedation   */
       // false uses a deterministic lifting scheme
@@ -17,6 +18,7 @@ export default class SkipList {
   // private fields
   #size
   #root
+  #depth
 
   // API
     constructor(options, ...data) {
@@ -36,13 +38,126 @@ export default class SkipList {
       this.config = Object.freeze(clone(options));
     }
 
+    get #depth() {
+      return Math.floor(
+        Math.log(this.#size) /
+        Math.log(1/this.config.p)
+      );
+    }
+
     get size() {
       return this.#size;
+    }
+
+    insert(thing) {
+      const liftUpdates = [];
+      let inserted = false;
+      let lastNode;
+      let node = this.#root;
+      let level = node.levelNext.length - 1;
+
+      while(!inserted && node !== undefined && level >= 0 &&) {
+        const next = node.levelNext[level];
+
+        // if there are no more nodes at this level
+        if ( next == undefined ) {
+          // go down
+          level -= 1;
+        }
+
+        const comparison = this.#compare(thing, next.thing);
+
+        if ( comparison > 0 ) { // if thing comes before next.thing
+          // go down
+          level -= 1;
+        } else if ( comparison < 0 ) { // if it comes after next.thing
+          // save last node
+          lastNode = node;
+          // save this node for possible update on lifting
+          liftUpdates[level] = node;
+          // go across
+          node = next; 
+        } else { // if it equals next thing
+          if ( this.config.dupeslicatesOkay ) { // and duplicates are OK
+            // save last node
+            lastNode = node;
+            // save this node for possible update on lifting
+            liftUpdates[level] = node;
+            // go across 
+            node = next;
+          } else { // if we don't allow duplicates
+            inserted = true;
+          }
+        }
+      }
+
+      if ( ! inserted ) {
+        const newNode = new Node({thing});
+
+        if ( node == undefined ) {
+          // we reached the end of the bottom path
+          
+
+          // for dev let's just check our condition is true
+          // that we ARE on the bottom path
+            if ( level !== 0 ) {
+              throw new TypeError(`Post loop path end can only occur on bottom path. 
+                Something is UP with your skiplist.
+              `);
+            }
+        
+          lastNode.setLevelNext(0, newNode);
+        } else if ( level < 0 ) {
+          // we are somewhere in the middle of the bottom path 
+
+          // so insert between node and node.next
+          const postNewNode = node.next;
+
+          node.setLevelNext(0, newNode);
+          newNode.setLevelNext(0, postNewNode);
+        }
+
+        inserted = true;
+
+        this.#liftUp(newNode, liftUpdates); // lift up the inserted node
+      }
+
+      return inserted;
+    }
+
+  // private instance methods
+    // life a node up to higher levels
+    #lift(node) {
+      if ( this.config.randomized ) {
+
+      } else {
+
+      }
+    }
+
+    // determine order between two things
+    #compare(aThing, bThing) {
+      if ( this.config.compare ) {
+        return this.config.compare(aThing, bThing);
+      } else {
+        // order comparison
+        if ( aThing > bThing ) {
+          return this.config.max ? 1 : -1;
+        } else if ( aThing == bThing ) {
+          return 0;
+        } else {
+          return !this.config.max ? 1 : -1;
+        }
+      }
     }
 
   // static methods
     static print(skiplist) {
       let row = 0;
+      
+
+      // print each level 
+
 
       console.log('\n\n');
     }
@@ -59,22 +174,22 @@ export function create(...args) {
 
 class Node {
   // private fields
-  #listNext
+  #levelNext
 
   constructor({thing} = {}) {
     Object.assign(this, {thing});
   }
 
-  get listNext() {
-    return Array.from(this.#listNext);
+  get levelNext() {
+    return Array.from(this.#levelNext);
   }
 
-  set listNext(nothing) {
+  set levelNext(nothing) {
     throw new TypeError(`Cannot set successors for all lists, use setNextAtList(i, node) instead.`);
   }
 
   setNext(i, node) {
-    return this.#listNext[i] = node;
+    return this.#levelNext[i] = node;
   }
 }
 
