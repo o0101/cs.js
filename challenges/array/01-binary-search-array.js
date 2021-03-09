3ec055c230e323c2515f3ec8148713a832ab0d14
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-03-09 07:44:40 +0000

    "OK"

---
+++ challenges/array/01-binary-search-array.js
@@ -0,0 +1,39 @@
+// iterative binary search
+let binarySearch = function(a, key) {
+  let low = 0;
+  let high = a.length;
+  // Note on the condition:
+    // <= is important as < 	
+    // will only handle down to cases where subarray a[low..high] is length 2
+    // but subarray of length 1 can only be handled by <=
+  while( low <= high ) {
+    const mid = (low+high)>>1;
+    const midKey = a[mid];
+    if ( midKey < key ) {
+      low = mid + 1;
+    } else if ( midKey > key ) {
+      high = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+};
+
+let binarySearchRec = function(a, key, low, high) {
+  if ( low > high ) {
+    return -1;
+  }
+
+  const mid = (low+high)>>1;
+  const midKey = a[mid];
+
+  if ( midKey > key ) {
+    return binarySearchRec(a, key, low, mid - 1);
+  } else if ( midKey < key ) {
+    return binarySearchRec(a, key, mid + 1, high);
+  } else {
+    return mid;
+  }
+}
+
---
+++ challenges/array/02-sliding-window-max.js
@@ -0,0 +1,70 @@
+// try 1 - O(nw) 
+let findMaxSlidingWindow1 = function(arr, window_size) {
+	const result = [];
+	const window = {
+		max: -Infinity,
+		index: -window_size,
+		list: []
+	}
+	
+	// if window.list was a heap or skiplist this would be O(N log W)
+	// but as it is it is O(NW) worst case
+	for( let i = 0; i < arr.length; i++ ) {
+		const item = arr[i];
+		window.list.push(item);
+		if ( window.list.length > window_size ) {
+			const leaving = window.list.shift();
+			if ( window.max === leaving ) {
+				window.max = item;
+				
+				window.list.forEach(wItem => {
+					if ( wItem > window.max ) {
+						window.max = wItem;
+					}
+				})
+			}
+		}
+		
+		if ( item > window.max ) {
+			window.max = item;
+		}
+		if ( window.list.length === window_size ) {
+			result.push(window.max);
+		}
+	}
+	return result;
+};
+
+// try 2 - using deque
+  // although in JS this deque is not truly 0(1) at both ends I think
+  // we could use a double linked list
+  // this challenge is good for me because I never thought deque could be used to do anything cool
+let ffindMaxSlidingWindow = function(arr, windowSize) {
+  const result = [];
+  const list = [];
+
+  // if window.list was a heap or skiplist this would be O(N log W)
+  // but as it is it is O(NW) worst case
+  for( let i = 0; i < arr.length; i++ ) {
+    const item = arr[i];
+    const queueBack = arr[list[list.length-1]];
+    if ( item > queueBack ) {
+      while(item > arr[list[list.length-1]]) {
+        list.pop();
+      }
+    }
+    const leaving = i - windowSize;
+    if ( leaving >= 0 ) {
+      if( leaving >= list[0] ) {
+        list.shift();
+      }
+    }
+    list.push(i);
+
+    if ( (i+1) >= windowSize ) {
+      result.push(arr[list[0]]);
+    }
+  }
+
+  return result;
+};
