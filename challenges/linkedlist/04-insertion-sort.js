57c26923300f7d298e0bf749b39d1c8f7d71c63b
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-03-14 11:04:25 +0000

    "All 3 possible types of solutions implemented for sort linked list with insertion sort.
    FUck yeah! :P ;) xx"

--- challenges/linkedlist/04-insertion-sort.js
+++ challenges/linkedlist/04-insertion-sort.js
@@ -12,6 +12,7 @@ import {
   test();
 
   function test(){
+    console.log(`\nTest at: ${new Date}`);
     test1();
   }
 
@@ -87,11 +88,11 @@ import {
   }
 }
 
-
 {
-  //test();
+  test();
 
   function test(){
+    console.log(`\nTest at: ${new Date}`);
     test1();
   }
 
@@ -112,7 +113,7 @@ import {
   }
 
   // O(n**2) algorithm below, with O(1) space
-  // my original solution
+  // a solution I wrote after reading the official solution
   function insertionSort(head) {
     let priorNode = head;
 
@@ -121,37 +122,12 @@ import {
     let node = head.next;
 
     while(node) {
-      // save data for convenience and next because it may be overwritten
-      const {data,next} = node;
+      // save next because it may be overwritten
+      const {next} = node;
 
       let alreadyInOrder = true;
 
-      // insert
-        let priorINode;
-        let inode = head;
-        while(inode !== node) {
-          if ( inode.data <= data ) {
-            priorINode = inode;
-            inode = inode.next; 
-          } else {
-            // splice out
-            priorNode.next = node.next;
-
-            // splice in
-            if ( priorINode ) {
-              priorINode.next = node;
-            } else {
-              head = node;
-            }
-            node.next = inode;
-
-            // dont update outerloop pointers
-            alreadyInOrder = false;
-
-            // slot found
-            break;
-          }
-        }
+      ({head, alreadyInOrder} = insertInOrder(head, priorNode, node));
 
       // if we didn't move it, then we can update priorNode
         // if we moved it already, node would no longer be the predecessor of
@@ -164,5 +140,97 @@ import {
     }
 
     return head;
+  }
+
+  function insertInOrder(head, priorNode, node) {
+    const {data} = node;
+    let inode = head;
+
+    let alreadyInOrder = true;
+    let priorINode;
+
+    while(inode !== node) {
+      if ( inode.data <= data ) {
+        priorINode = inode;
+        inode = inode.next; 
+      } else {
+        // splice out
+        priorNode.next = node.next;
+
+        // splice in
+        if ( priorINode ) {
+          priorINode.next = node;
+        } else {
+          head = node;
+        }
+        node.next = inode;
+
+        // dont update outerloop pointers
+        alreadyInOrder = false;
+
+        // slot found
+        break;
+      }
+    }
+
+    return {head, alreadyInOrder}
+  }
+}
+
+{
+  test();
+
+  function test(){
+    console.log(`\nTest at: ${new Date}`);
+    test1();
+  }
+
+  function test1() {
+    let runs = 5;
+    do {
+      console.log(`\n${runs} runs remaining...`);
+      const randomList = randomNumberList(30);
+      let list = createLinkedList(randomList);
+      console.log(`Before insertion sort: ${display(list)}`);
+
+      list = insertionSort(list);
+
+      console.log(`After insertion sort: ${display(list)}`);
+    } while(--runs);
+
+    console.log(`Done!`);
+  }
+
+  // O(n**2) algorithm below, with O(1) space
+  // a solution I wrote after thinking about the official solution more
+  function insertionSort(head) {
+    let sorted = null;
+    let curr = head;
+
+    while(curr) {
+      const {next} = curr;
+      sorted = insertInOrder(sorted, curr);
+      curr = next;
+    }
+
+    return sorted;
+  }
+
+  function insertInOrder(sorted, node) {
+    if (!sorted || node.data <= sorted.data) {
+      node.next = sorted;
+      return node;
+    }
+
+    let curr = sorted;
+
+    while(curr.next && curr.next.data <= node.data) {
+      curr = curr.next;
+    }
+
+    node.next = curr.next;
+    curr.next = node;
+
+    return sorted;
   }
 }
