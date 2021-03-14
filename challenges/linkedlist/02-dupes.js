228ca4e7053ad7186e75d3ad1e502eeb55dd11de
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-03-14 01:22:03 +0000

    "List good"

---
+++ challenges/linkedlist/02-dupes.js
@@ -0,0 +1,46 @@
+import {
+  LinkedListNode,
+  createRandomLinkedList,
+  display
+} from './lib.js';
+
+test();
+
+function test() {
+  let runs = 5;
+  do {
+    console.log(`${runs} runs remaining...`);
+    let list = createRandomLinkedList(100);
+    console.log(`Before dedupe: ${display(list)}`);
+
+    list = dedupe(list);
+
+    console.log(`After dedupe: ${display(list)}`);
+  } while(--runs);
+
+  console.log(`Done!`);
+}
+
+// O(n) algorithm below, with O(n) space
+  // if O(1) space is desired two approaches are possible:
+    // 1 - Sort first, and then remove dupes in linear scan O(n log n)
+    // 2 - For each node, scan the nodes before it to see if it occured before. O(n**2)
+function dedupe(head) {
+  const values = new Set();
+  let lastNode = null;
+  let node = head;
+
+  while(node) {
+    if ( values.has(node.data) ) {
+      if ( lastNode ) {
+        lastNode.next = node.next;
+      }
+    } else {
+      values.add(node.data);
+    }
+    lastNode = node;
+    node = node.next;
+  }
+
+  return head;
+}
---
+++ challenges/linkedlist/03-delete-key.js
@@ -0,0 +1,76 @@
+import {
+  LinkedListNode,
+  createLinkedList,
+  display
+} from './lib.js';
+import {
+  randomNumberList,
+  randomItem
+} from '../../src/test.js';
+
+test();
+
+function test(){
+  test1();
+  test2();
+}
+
+function test1() {
+  let runs = 5;
+  do {
+    console.log(`${runs} runs remaining...`);
+    const randomList = randomNumberList(30);
+    let key = randomItem(randomList);
+    let list = createLinkedList(randomList);
+    //const key = pickRandomNode(list).data;
+    console.log(`Before delete: ${display(list)}`);
+
+    if (runs === 1) {
+      key = 0;
+    }
+
+    console.log(`Key to delete: ${key}`);
+
+    list = deleteKey(list, key);
+
+    console.log(`After delete: ${display(list)}`);
+  } while(--runs);
+
+  console.log(`Done!`);
+}
+
+function test2() {
+  let key = 0;
+  let list = createLinkedList([1,2,3,0,1,2,3,0,5,6,7,0,0,0,9,9,9,0,0,0]);
+  //const key = pickRandomNode(list).data;
+  console.log(`Before delete: ${display(list)}`);
+
+  console.log(`Key to delete: ${key}`);
+
+  list = deleteKey(list, key);
+
+  console.log(`After delete: ${display(list)}`);
+}
+
+// O(n) algorithm below, with O(1) space
+function deleteKey(head, key) {
+  let lastNode = null;
+  let node = head;
+
+  while(node) {
+    if ( key === node.data ) {
+      if ( lastNode ) {
+        lastNode.next = node.next;
+      } else if ( head === node ) {
+        head = node.next;
+      }
+    } else {
+      // careful here, only update lastNode if we did not delete node!
+      // otherwise the last, lastNode = node node will still be in list
+      lastNode = node;
+    }
+    node = node.next;
+  }
+
+  return head;
+}
