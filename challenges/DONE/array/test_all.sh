9ea31a77dee27c100b7e32611b89f193c2a6c401
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-03-20 03:24:14 +0000

    "OK"

---
+++ challenges/DONE/array/test_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+node -r esm 01-binary-search-array.js
+node -r esm 02-sliding-window-max.js
+node -r esm 03-search-rotated-array.js
+node -r esm 04-arrays-smallest-common.js
+node -r esm 05-rotate-array.js
+node -r esm 06-low-high-indices.js
+node -r esm 07-move-zeroes.js
+node -r esm 08-max-pro.js
+node -r esm 09-merge-intervals.js
+node -r esm 10-2-subset-sum.js
+node -r esm 11-quick-sort.js
