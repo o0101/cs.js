f29f1a7f3ce6a388cfff14f4b8a92700766192f9
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-03-23 12:09:35 +0000

    "Partition"

--- challenges/math-and-stats/04-partition-problem.js
+++ challenges/math-and-stats/04-partition-problem.js
@@ -1,4 +1,4 @@
-console.log(partitions(6));
+console.log(partitions(15));
 
 function partitions(n, k = 1) {
   const p = [];
@@ -7,15 +7,14 @@ function partitions(n, k = 1) {
     //console.log(JSON.stringify({p}));
     a(n, i, p); 
   }
-  //console.log(JSON.stringify({p}));
-  return p;
+  console.log(JSON.stringify({p}));
+  return p.sort();
 }
-
 
 function a(n, i, list) {
   if ( i >= n ) return;
   //console.log({a:{n,i,list}});
-  list.push([n-i, i]);
-  const b = partitions(n-i, i).map(p => (p.push(i),p));
+  list.push([i, n-i]);
+  const b = partitions(n-i, i).map(p => (p.push(i),p.sort(),p));
   list.push(...b);
 }
