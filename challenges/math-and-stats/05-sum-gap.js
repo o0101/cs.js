4353a56e5846a544843a2bca23c722ca72e8a019
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-03-23 13:29:29 +0000

    OK

--- challenges/math-and-stats/05-sum-gap.js
+++ challenges/math-and-stats/05-sum-gap.js
@@ -1,8 +1,12 @@
 // i like this because i saw the solution in my head in about 5 seconds
 
+console.log(sumGap([3, 7, 1, 2, 8, 4, 5]));
+
 function sumGap(arr) {
-  const n = arr.length;
-  const targetSum = (n**2 + n)/2;
-  const actualSum = arr.reduce((S,v) => S + v, 0);
+  // + 1 because 1 number is missing, 
+  // and there will be numbers 1...n present (less that missing guy)
+  const n = arr.length + 1;
+  const targetSum = (n**2 + n)/2; // this is the equation for a serial sum
+  const actualSum = arr.reduce((S,v) => S + v, 0); 
   return targetSum - actualSum;
 }
