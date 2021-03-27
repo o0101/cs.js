9ae54e0286b3e439b98319e43e620acd8b45dd38
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-03-27 05:50:43 +0000

    "Good lesson"

--- challenges/04-strings/01-reverse-word-order.js
+++ challenges/04-strings/01-reverse-word-order.js
@@ -23,43 +23,63 @@ let rwo = function reverseWordOrder(s) {
 
 // this is complex, reminds me to always keep things simple
 rwo = function hardReverseWordOrder(s) {
-  WS = /\s/g;
-  const sentenceParts = [];
-  const part = {
-    isWord: false,
-    chars: []
-  };
+	WS = /\s/g;
+	const sentenceParts = [];
+	const part = {
+		isWord: false,
+		chars: []
+	};
 
-  for( const char of s ) {
-    switch(WS.test(char)) {
-      case false:
-        if ( !part.isWord ) {
-          sentenceParts.push(part.chars.join(''));
-          part.chars = [char];
-          part.isWord = true;
-        } else {
-          part.chars.push(char);
-        }
-        break;
+	for( const char of s ) {
+		switch(WS.test(char)) {
+			case false:
+				if ( !part.isWord ) {
+					sentenceParts.push(part.chars.join(''));
+					part.chars = [char];
+					part.isWord = true;
+				} else {
+					part.chars.push(char);
+				}
+				break;
 
-      case true:
-        part.chars.push(char);
-        if ( part.isWord ) {
-          sentenceParts.push(part.chars.join(''));
-          part.chars = [char];
-          part.isWord = false;
-        } else {
-          part.chars.push(char);
-        }
-        break;
+			case true:
+				if ( part.isWord ) {
+					sentenceParts.push(part.chars.join(''));
+					part.chars = [char];
+					part.isWord = false;
+				} else {
+					part.chars.push(char);
+				}
+				break;
+		}
+	}
 
-    }
-  }
+	sentenceParts.push(part.chars.join(''));
 
-  sentenceParts.push(part.chars.join(''));
+	sentenceParts.reverse();
 
-  sentenceParts.reverse();
-
-  return sentenceParts.join('');
+	return sentenceParts.join('');
 }
 
+// wow the above was a real lesson for me in keeping things simple
+// the more code, the more change for errors
+// mistake 1. i didn't correctly put the chars += char behind the else side of the 
+// part.isWord conditional meaning I was misaligning the character at the word-whitespace interface
+// mistake 2. I forgot to add the last part to sentenceParts meaning
+// i was missing the last (first, in reversed order) word
+// mistake 3. I left a line chars += char inside the whitespace case in the switch block
+// outside the conditional so I was adding extra whitespace to the word before switching to a 
+// whitspace segment
+
+
+// wow amazing 3 simple mistakes
+// but hard to catch or understand
+// because they slipped past me
+// because even this simple problem
+// if I wrote it in a long way, could introduce bugs
+// the more lines I wrote, the more bugs
+// the more bugs, the more I needed to fix and change code
+// and the more bugs. more bugs could come while doing that. while fixing. 
+// it's really a lesson for me to keep things simple, or be very deliberate if I'm making it long
+// I mean only if I'm in a test, otherwise I can just hack it out, but if I'm in a test
+// it pays to be very deliberate
