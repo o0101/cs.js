f0bbb2c575211d0f96d62bdd98531ece50a6fa59
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-04-07 02:25:31 +0000

    "Up to 20"

--- coding/leetcode/14-longest-prefix.js
+++ coding/leetcode/14-longest-prefix.js
@@ -20,3 +20,16 @@ var longestCommonPrefix = function(strs) {
   
   return lcp;
 }; 
+
+longestCommonPrefix = function(strs) {
+  if ( strs.length === 0 ) return "";
+
+  let prefix = strs[0];
+  for( let i = 1; i < strs.length; i++ ) {
+    while( strs[i].indexOf(prefix) !== 0 ) {
+      prefix = prefix.substr(0, prefix.length - 1);
+    }
+    if ( prefix.length === 0 ) break;
+  }
+  return prefix;
+}
