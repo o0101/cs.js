71794a975c6216d572ce29f0a7ef3b5aa69a6d85
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-04-10 02:34:35 +0000

    "OK"

--- coding/extra-challenges/05-trees/02-inorder-iterator.js
+++ coding/extra-challenges/05-trees/02-inorder-iterator.js
@@ -40,6 +40,49 @@ let InorderIterator = class InorderIterator {
   }
 }
 
+// Below is my second solution I created after reading about their solution
+// using a pre-prepared single stack
+let InorderIterator = class InorderIterator {
+  constructor(root) {
+    this.stack = [...allLeft(root)];
+  }
+  hasNext() {
+   return this.addLeft.length || this.addRight.length;
+  }
+  // getNext returns null if there are no more elements in tree
+  getNext() {
+    //console.log({left:this.addLeft.map(n => n.data), right:this.addRight.map(n => n.data)});
+    if ( this.addLeft.length ) {
+      let node = this.addLeft.pop();
+      while(node.left) {
+        this.addRight.push(node);
+        if ( node.left ) {
+          this.addLeft.push(node.left);
+        }
+        node = this.addLeft.pop();
+      }
+      if ( node.right ) {
+        this.addRight.push(node.right);
+      }
+      //console.log({node:node.data});
+      return node;
+    } else if ( this.addRight.length ) {
+      let node = this.addRight.pop();
+      if ( node.right ) {
+        this.addLeft.push(node.right);
+      }
+      //console.log({node:node.data});
+      return node;
+    }
+  }
+}
+
+function allLeft(node) {
+  while(node) {
+    
+  }
+}
+
 let inorderUsingIterator = function(root) {
   displayLevelOrder(root);
   let iter = new InorderIterator(root);
