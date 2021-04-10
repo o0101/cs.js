8624674c634c6e8924948d6c8838617c189f39c4
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-04-10 02:31:38 +0000

    "OK"

---
+++ coding/extra-challenges/05-trees/02-inorder-iterator.js
@@ -0,0 +1,52 @@
+// This iterative in order is interesting
+// below is my first solution
+// using two stacks that I kind of crafted and hacked out by hand
+// but I'd prefer a simpler solution
+// that I felt sure would work in every case (this one I'm not!)
+
+let InorderIterator = class InorderIterator {
+  constructor(root) {
+    this.addLeft = [root];
+    this.addRight = [];
+  }
+  hasNext() {
+   return this.addLeft.length || this.addRight.length;
+  }
+  // getNext returns null if there are no more elements in tree
+  getNext() {
+    //console.log({left:this.addLeft.map(n => n.data), right:this.addRight.map(n => n.data)});
+    if ( this.addLeft.length ) {
+      let node = this.addLeft.pop();
+      while(node.left) {
+        this.addRight.push(node);
+        if ( node.left ) {
+          this.addLeft.push(node.left);
+        }
+        node = this.addLeft.pop();
+      }
+      if ( node.right ) {
+        this.addRight.push(node.right);
+      }
+      //console.log({node:node.data});
+      return node;
+    } else if ( this.addRight.length ) {
+      let node = this.addRight.pop();
+      if ( node.right ) {
+        this.addLeft.push(node.right);
+      }
+      //console.log({node:node.data});
+      return node;
+    }
+  }
+}
+
+let inorderUsingIterator = function(root) {
+  displayLevelOrder(root);
+  let iter = new InorderIterator(root);
+  let result_str = '';
+  while (iter.hasNext()) {
+    let ptr = iter.getNext();
+    result_str += ptr.data + ' ';
+  }
+  return result_str;
+};
