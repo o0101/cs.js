c9d4f7e7c224ea24ce56d657abbed45179574b12
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-04-11 05:07:40 +0000

    "OK"

---
+++ coding/extra-challenges/05-trees/04-inorder-successor.js
@@ -0,0 +1,49 @@
+/**
+	My first solution.
+
+  Each step:
+  - Search for d in root
+  - Get that node
+  - Also set up parents
+  - Then go up parent chain to find smallest parent larger than d if no right
+  - Else go down the right chain at the left to find smallest guy above d
+
+  Seems I can simplify the first phase:
+  Search, can probably be DFS which will store the parent chain.
+  So I can basically find the successor and find d in parent chain in 1 step. 
+  But if it has a right path then I just find d.
+
+**/
+let inorderSuccessorBST = function(root, d) {
+  let val = d;
+    const stack = [root];
+    while(stack.length) {
+      const node = stack.pop();
+      if ( node.data == val ) {
+        d = node;
+      }
+      if ( node.left ) {
+        node.left.parent = node;
+        stack.push(node.left);
+      }
+      if ( node.right ) {
+        node.right.parent = node;
+        stack.push(node.right);
+      }
+    }
+  if ( ! d ) return;
+  if ( d.right ) {
+    console.log('x',d.data,d.right.data);
+    d = d.right;
+    while(d.left && (d.left.data > val)) {
+      d = d.left;
+    }
+    return d.left || d;
+  } else if ( d.parent ) {
+    console.log('y',d.data,d.parent.data);
+    while(d.parent && (d.parent.data < d.data)) {
+      d = d.parent;
+    }
+    return d.parent;
+  }
+};
