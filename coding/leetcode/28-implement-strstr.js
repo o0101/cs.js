d6ed5bff09f8a792f84bdf0d76f608269f8fd5f2
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-04-15 03:18:35 +0000

    "Great"

---
+++ coding/leetcode/28-implement-strstr.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  const hLen = haystack.length;
+  const nLen = needle.length;
+  // pattern state
+  let fStart = -1; 
+  let nStart = 0;
+  
+  // source state
+  let hStart = 0;
+  
+  if ( nLen === 0 ) return 0;
+  if ( nLen > hLen ) return -1;
+  
+  while(hStart < hLen) {
+    const hc = haystack[hStart];
+    const nc = needle[nStart];
+    
+    if ( hc !== nc ) {
+      if ( fStart !== -1 ) {
+        hStart = fStart;
+        fStart = -1;
+      }
+      nStart = 0;
+    } else {
+      if ( fStart === -1 ) {
+        fStart = hStart; 
+      }
+      nStart++;
+      if ( nStart >= nLen ) {
+        return fStart;
+      }
+    }
+    hStart++;
+  }
+  
+  if ( nStart >= nLen ) {
+    return fStart;
+  }
+  return -1;
+};
