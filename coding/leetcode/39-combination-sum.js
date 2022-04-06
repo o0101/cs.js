03132ab274b5de4e23ce4e96a127e785012f331d
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-04-06 22:24:57 +0800

    Works but it should be faster

--- coding/leetcode/39-combination-sum.js
+++ coding/leetcode/39-combination-sum.js
@@ -1,5 +1,48 @@
-var combinationSum = function(candidates, target) {
-  candidates = candidates.map(n => parseInt(n));
-  candidates.sort((a,b) => a-b); // increasing order makes precomputation easier
+var combinationSum = function(c, t) {
+  const C = c.length;
+  c = c.map(n => parseInt(n));
+  c.sort((a,b) => b-a); // decreasing order 
+  const m = [];
+  const P = [];
+  let i = 0;
+  while(i < C) {
+    const sum = [];
+    let N = t;
+    let j = i;
+    while(j < C) {
+      const part = c[j]; 
+      while(part <= N) {
+        N -= part;
+        sum.push(part);
+        m.push(...combinationSum(c.slice(j+1), N).map(x => (x.push(...sum), x)));
+      }
+      j++;
+    }
+    if ( N === 0 ) {
+      m.push(sum);
+    }
+    i++;
+  }
+  const dup = new Set();
+  m.forEach(x => x.sort());
+  const result = m.filter(y => {
+    const key = y.join(',');
+    if ( !dup.has(key) ) {
+      dup.add(key); 
+      return true;
+    }
+    return false;
+  });
+  return result;
 };
+
+/*
+const T = [
+  combinationSum([2], 1),
+  combinationSum([2,3,5], 8),
+  combinationSum([2,3,6,7], 7),
+];
+
+console.log(T);
+*/
 
