f3dd54b3f4270e9bbb52479376ae7ac4e9a72340
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-04-09 00:01:11 +0800

    Confirmed multiply algorithm worked on RSA-250 ;p ;) xx ;p hehe ;P xxx;p

--- coding/leetcode/daily/April-8-2022-kth-largest.js
+++ coding/leetcode/daily/April-8-2022-kth-largest.js
@@ -1,21 +1,94 @@
-class KLarge {
-  /**
-   * @param {number} k
-   * @param {number[]} nums
-   */
-  constructor(k, nums) {
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
 
-
+class BH {
+  constructor() {
+    this.values = [];
   }
+  add(element) {
+    this.values.push(element);
+    let index = this.values.length - 1;
+    const current = this.values[index];
 
-  /** 
-   * @param {number} val
-   * @return {number}
-   */
-  add(val) {
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
 
+      if (parent >= current) {
+        this.values[parentIndex] = current;
+        this.values[index] = parent;
+        index = parentIndex;
+      } else break;
+    }
   }
+  pop() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+
+    let index = 0;
+    const length = this.values.length;
+    const current = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild < current) swap = leftChildIndex;
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild < current) ||
+          (swap !== null && rightChild < leftChild)
+        )
+          swap = rightChildIndex;
+      }
+
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = current;
+      index = swap;
+    }
+
+    return max;
+  }
+}
+
+
+var KthLargest = function(k, nums) {
+  this.heap = new BH();
+  nums.forEach(n => this.heap.add(n));
+  this.k = k;
+  
 };
 
-var KthLargest = KLarge
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+  this.heap.add(val);
+  while ( this.heap.values.length > this.k ) {
+    //console.log('add', val, 'size', this.heap.values);
+    this.heap.pop();
+  }
+  const kth = this.heap.values[0];
+  return kth;
+};
 
+/** 
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
+
+function clone(o) {
+  return JSON.parse(JSON.stringify(o));
+}
+ 
